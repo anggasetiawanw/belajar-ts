1bd6d8732ce9ed91893d15d1b784d2121e9351f1
anggasetiawanw <anggasetiaw@gmail.com>
2024-02-18 00:56:45 +0700

    feat: added decorator and metadata in express

--- express/build/routes/loginRoutes.js
+++ express/build/routes/loginRoutes.js
@@ -1,68 +1,33 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.router = void 0;
 const express_1 = require("express");
 function requireAuth(req, res, next) {
-    return __awaiter(this, void 0, void 0, function* () {
-        if (req.session && req.session.loggedIn) {
-            next();
-            return;
-        }
-        res.status(403).json({ error: 'notpermitted' });
-        // res.send('Notetete');
-        // return res.redirect('/login');
-    });
+    if (req.session && req.session.loggedIn) {
+        next();
+        return;
+    }
+    res.status(403);
+    res.send('Not permitted');
 }
 const router = (0, express_1.Router)();
 exports.router = router;
 router.get('/', (req, res) => {
     if (req.session && req.session.loggedIn) {
-        res.send(`<div>
-        <div> You are Logged In </div>
-        <a href ="/logout">logout</a>
+        res.send(`
+      <div>
+        <div>You are logged in</div>
+        <a href="/logout">Logout</a>
       </div>
-      `);
+    `);
     }
     else {
-        res.send(`<div>
-        <div> You are not Logged In </div>
-        <a href ="/login">login</a>
-      </div>
-      `);
-    }
-});
-router.get('/login', (req, res) => {
-    res.send(`
-    <form method="POST">
-      <div> 
-        <label> Email </label>
-        <input name="email"/>
-      </div>
+        res.send(`
       <div>
-        <label> Password </label>
-        <input name="password" type="password"/>
+        <div>You are not logged in</div>
+        <a href="/login">Login</a>
       </div>
-      <button>Submit </button>
-    </form>
-  `);
-});
-router.post('/login', (req, res) => {
-    const { email, password } = req.body;
-    if (email &&
-        password &&
-        email === 'hi@tiktok.com' &&
-        password === 'password') {
-        req.session = { loggedIn: true };
-        res.redirect('/');
+    `);
     }
 });
 router.get('/logout', (req, res) => {
@@ -70,5 +35,5 @@ router.get('/logout', (req, res) => {
     res.redirect('/');
 });
 router.get('/protected', requireAuth, (req, res) => {
-    res.send('welcome to protecred path');
+    res.send('Welcome to protected route, logged in user');
 });
--- express/src/index.ts
+++ express/src/index.ts
@@ -1,5 +1,16 @@
-import { Server } from './server';
+import express, { Request, Response } from 'express';
+import bodyParser from 'body-parser';
+import cookieSession from 'cookie-session';
+import { AppRouter } from './AppRouter';
+import './controllers/LoginController';
+import './controllers/RootController';
 
-const server = new Server();
+const app = express();
 
-server.start();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieSession({ keys: ['laskdjf'] }));
+app.use(AppRouter.getInstance());
+
+app.listen(3000, () => {
+  console.log('Listening on port 3000');
+});
